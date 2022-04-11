55ff598edddb92dfffe0508cd0e53776cc4158d8
samu1243 <samueldjekki@gmail.com>
2022-04-11 17:33:20 -0400

    creacion del excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function arreglo(arr) {
+  let random = ~~(Math.random() * arr.lenght);
+  return arr[random];
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let resultado = `${arreglo(who)} ${arreglo(action)} ${arreglo(
+    what
+  )} ${arreglo(when)}`;
+  let element = document.getElementById("excusa");
+  element.innerHTML = resultado;
+  console.log(resultado);
 };
